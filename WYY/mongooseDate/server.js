2ed38db5d13b2209ce0dd3f574d8996d64091865
zengts <wananshine@Hotmail.com>
2018-03-15 17:50:53 +0800

    update

--- WYY/mongooseDate/server.js
+++ WYY/mongooseDate/server.js
@@ -82,7 +82,15 @@ app.get('/deleteUser', function (req, res) {
 var apiDate = require('./users.json');
 var goodsList = apiDate.goodsList;
 var banners = apiDate.banners;
+var welfareInfo = apiDate.welfareInfo;
 var floors = apiDate.floors;
+var albums = apiDate.albums;
+var albumProduct = apiDate.albumProduct;
+var getsHot = apiDate.getsHot;
+var shopingCart = apiDate.cart;
+var typesList = apiDate.typesList;
+var category_v2 = apiDate.category_v2;
+var toPay = apiDate.toPay;
 var apiRoutes = express.Router();
 
 
@@ -102,12 +110,100 @@ apiRoutes.get('/floors',function(req, res){
 	})
 })
 
+
+//最后一个floors列表(音乐专辑)
+apiRoutes.get('/albums',function(req, res){
+  res.json({
+      errno:0,
+      data:albums
+  })
+})
+
+//最后一个floors列表(音乐专辑)其他的product
+apiRoutes.get('/albumProduct',function(req, res){
+  res.json({
+      errno:0,
+      data:albumProduct
+  })
+})
+
+//分类列表（网易云商城数据）
+apiRoutes.get('/typesList',function(req, res){
+  res.json({
+    errno: 0,
+    data:typesList
+  })
+})
+
+//分类列表(小米的数据)
+apiRoutes.get('/category_v2', function(req, res){
+  res.json({
+    errno: 0,
+    data: category_v2
+  })
+})
+
+//购物车列表
+apiRoutes.get('/shopingCart',function(req, res){
+  res.json({
+    errno:0,
+      data:shopingCart
+  })
+})
+
 //商品列表
 apiRoutes.get('/goodslist',function(req, res){
 	res.json({
 		errno:0,
     	data:goodsList
 	})
+})
+
+
+//热门商品
+apiRoutes.get('/getshot',function(req, res){
+  res.json({
+    errno:0,
+    data:getsHot
+  })
+})
+
+//福利社(welfare)
+apiRoutes.get('/welfareinfo', function(req, res){
+  res.json({
+    errno: 0,
+    data: welfareInfo
+  })
+})
+
+
+
+//待支付
+apiRoutes.get('/topay', function(req, res){
+  res.json({
+    errno: 0,
+    data: toPay
+  })
+})
+
+
+
+//拿到指定热门商品id的商品信息
+apiRoutes.get('/getshot/:goodsId',function(req, res){
+  var goodsId = req.params.goodsId;
+  var hotPt = getsHot.data.hotProduct;
+  function selectedGood(goodsId){
+    for (var i = 0; i < hotPt.length; i++) {
+      if(hotPt[i].products.id == goodsId){
+        return hotPt[i];
+      };
+    }
+  }
+  var Pt = selectedGood(goodsId);
+  res.json({
+    errno:0,
+      data:Pt
+  })
 })
 
 
@@ -120,6 +216,8 @@ apiRoutes.get('/:goods_id', function (req, res) {
 	})
    res.end( JSON.stringify(user));
 })
+
+
 
 app.use('/api',apiRoutes);
 
